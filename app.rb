d3eb57994ee2983d7c9eeb77136b12e84384149c
jtflink <jordan.flink@kellogg.northwestern.edu>
2020-03-15 19:14:06 +0000

    Update

--- app.rb
+++ app.rb
@@ -14,5 +14,153 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+places_table = DB.from(:places)
+recommendations_table = DB.from(:recommendations)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+# index
+get "/" do
+    puts "params: #{params}"
+
+    @places = places_table.all.to_a
+    pp @places
+
+    view "places"
+end
+
+# place details
+get "/places/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @place = places_table.where(id: params[:id]).to_a[0]
+    pp @place
+
+    @recommendations = recommendations_table.where(place_id: @place[:id]).to_a
+    @recommendations_count = recommendations_table.where(event_id: @place[:id], going: true).count
+
+    view "place"
+end
+
+# display the recommendations form
+get "/places/:id/recommendations/new" do
+    puts "params: #{params}"
+
+    @place = places_table.where(id: params[:id]).to_a[0]
+    view "new_recommendation"
+end
+
+# receive the submitted recommendations form
+post "/places/:id/recommendations/create" do
+    puts "params: #{params}"
+
+    @place = places_table.where(id: params[:id]).to_a[0]
+    recommendations_table.insert(
+        place_id: @place[:id],
+        user_id: session["user_id"],
+        recommendations: params["recommendations"]
+    )
+
+    redirect "/places/#{@place[:id]}"
+end
+
+# display the recommendations form
+get "/recommendations/:id/edit" do
+    puts "params: #{params}"
+
+    @recommendation = recommendations_table.where(id: params["id"]).to_a[0]
+    @place = places_table.where(id: @recommendation[:place_id]).to_a[0]
+    view "edit_recommendation"
+end
+
+# receive the submitted recommendation form
+post "/recommendations/:id/update" do
+    puts "params: #{params}"
+
+    @recommendation = recommendations_table.where(id: params["id"]).to_a[0]
+    @place = places_table.where(id: @recommendation[:place_id]).to_a[0]
+
+    if @current_user && @current_user[:id] == @recommendation[:id]
+        recommendations_table.where(id: params["id"]).update(
+            recommendations: params["recommendations"]
+        )
+
+        redirect "/places/#{@place[:id]}"
+    else
+        view "error"
+    end
+end
+
+# delete the recommendation
+get "/recommendations/:id/destroy" do
+    puts "params: #{params}"
+
+    recommendation = recommendations_table.where(id: params["id"]).to_a[0]
+    @place = places_table.where(id: recommendation[:place_id]).to_a[0]
+
+    recommendations_table.where(id: params["id"]).delete
+
+    redirect "/places/#{@place[:id]}"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    # if there's already a user with this email, skip!
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+
+        redirect "/logins/new"
+    end
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
+end
+
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,30 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :places do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
-  String :location
+  String :address
 end
-DB.create_table! :rsvps do
+DB.create_table! :recommendations do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :place_id
+  foreign_key :user_id
+  String :recommendations, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+recommendations_table = DB.from(:recommendations)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+recommendations_table.insert(title: "Bennisons", 
+                    description: "The best bakery in Evanston",
+                    address: "Somewhere")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+puts "Success!" 
