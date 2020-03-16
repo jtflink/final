5dbc6861d1f3fe16777074ea72ad2b2d6bad681b
jtflink <jordan.flink@kellogg.northwestern.edu>
2020-03-16 21:40:23 +0000

    a meaningful commit message

--- createdb.rb
+++ createdb.rb
@@ -25,39 +25,14 @@ end
 
 # Insert initial (seed) data
 places_table = DB.from(:places)
-recommendations_table = DB.from(:recommendations)
-users_table = DB.from(:users)
 
-places_table.insert(id: 0,
-                    title: "Evanston",
+places_table.insert(title: "Evanston",
                     lat_long: "42.0451,-87.6877")
 
-places_table.insert(id: 1,
-                    title: "Chicago",
+places_table.insert(title: "Chicago",
                     lat_long: "41.8781,-87.6298")
 
-places_table.insert(id: 2,
-                    title: "San Francisco",
+places_table.insert(title: "San Francisco",
                     lat_long: "37.7749,-122.4194")
-
-recommendations_table.insert(id: 0,
-                    place_id: 0,
-                    user_id: 0,
-                    recommendations: "There are so many things to do in Evanston!")
-
-recommendations_table.insert(id: 1,
-                    place_id: 1,
-                    user_id: 0,
-                    recommendations: "Chicago, the Windy City, has so many things to do. One of the best things to do in the city is to visit Navy Pier.")
-
-recommendations_table.insert(id: 2,
-                    place_id: 2,
-                    user_id: 0,
-                    recommendations: "One of the best things to do in San Francisco is to visit Golden Gate Park.")
-
-users_table.insert(id: 0,
-                    name: "user1", 
-                    email: "user1@user1.com",
-                    password: "user1")
 
 puts "Success!" 
